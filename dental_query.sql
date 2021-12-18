845c94a34883ae5266ec2375f1d71186799d468c
bhanuprathapredyn <47811452+bhanuprathapreddyn@users.noreply.github.com>
2021-12-17 22:31:39 -0500

    Add files via upload

---
+++ dental_query.sql
@@ -0,0 +1,31 @@
+insert into Doctor values('DD111','Natasha',8745632,'Orthodontics');
+insert into patient values('DP010','Clair',4782964,33,'los angeles');
+
+update patient set age=38 where patient_id='DP010';
+update Doctor set doctor_number=6783434 where doctor_Id='DD111';
+
+delete from Doctor where doctor_id='DD111';
+
+select * from Treatment;
+
+select  a.patient_id, p.patient_name
+from patient p
+inner join  Appointment a on a.patient_id = p.patient_id;
+
+select  a.Doctor_Id, d.Doctor_name
+from Doctor d
+inner join  Appointment a on a.Doctor_Id= d.Doctor_Id;
+
+Select count(patient_id),address from patient
+group by address order by count(patient_id);
+
+select count(appointment_id), appointment_date from Appointment
+group by appointment_date;
+
+select patient_id from patient union
+select patient_id from Appointment;
+
+select p.patient_name, a.appointment_date from patient p, Appointment a
+where p.patient_id=a.patient_id;
+
+select * from Transactions;
